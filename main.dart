33ecbe5818da1d22ecaa783f6f798533757e3f01
Roman <140193303+Belxz777@users.noreply.github.com>
2023-12-28 15:56:40 +0400

    Darty

--- main.dart
+++ main.dart
@@ -12,3 +12,10 @@ print(DateTime.now());
 
     print(image.values);
 }
+//  to install dart sdk you need to follow this steps:
+// run this if you are using windows choco install dart-sdk else visit this website  https://dart.dev/get-dart
+// after that you need to initialize it in you environment VARIABLES just select  a path and edit that 
+    // so add a route to your sdk by default it is C:\tools\dart-sdk 
+    // run dart --version to see the version of installed dart and to make sure that dart was completly installed 
+    // last step is to install an extension of dart 
+    //happy code!!!
