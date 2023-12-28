1427a382df8808b7a09a62fa8c23f8c9652bdc0c
Roman <140193303+Belxz777@users.noreply.github.com>
2023-12-28 15:50:44 +0400

    DartPlaygroundWithExamples

---
+++ main.dart
@@ -0,0 +1,14 @@
+late  String text;
+void main (){
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+text = "hello iam a predefined variable";
+var  time = DateTime.now();
+print(DateTime.now());
+    print(flybyObjects[0]);
+
+    print(image.values);
+}
